a1ede808157facaf487ddc8b95a99f564060f817
Ahmed M. Khallaf <ahmed.mostafa1198@gmail.com>
2020-05-12 21:04:54 +0200

    Feat: Adding header & routes

---
+++ src/components/Header/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { AppBar, Toolbar, Typography, Button, withStyles } from '@material-ui/core';
+
+import Link from '../_common/Link';
+import routes from '../../constants/routes';
+
+const styles = (theme) => ({
+  title: {
+    flexGrow: 0.7,
+  },
+  link: {
+    color: 'black',
+    paddingRight: theme.spacing(4),
+  },
+});
+
+
+const Header = ({ classes }) => (
+  <AppBar>
+    <Toolbar>
+        <Typography variant="h6" className={classes.title}>
+          Ahmed Khallaf
+        </Typography>
+        {routes.map(({ path, name }) => (
+          <Link key={name} href={path} className={classes.link}>
+            {name}
+          </Link>
+        ))}
+    </Toolbar>
+  </AppBar>
+);
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(Header);
---
+++ src/constants/routes.js
@@ -0,0 +1,5 @@
+export default [
+  { name: 'Home', path: '/' },
+  { name: 'Projects', path: '/#projects' },
+  { name: 'Contact', path: '/#contact' },
+]
